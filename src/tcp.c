c6017361fa70c25f638a520e87c436d079ea3f72
Agustin Petrini <agustin@sid-8.com>
2025-08-25 17:20:04 +0900

    Fix tcp bug preventing accepting clients

--- src/tcp.c
+++ src/tcp.c
@@ -12,7 +12,7 @@ server_status_e bind_tcp_port(tcp_server *server, int port)
     server->socket_fd = socket(AF_INET, SOCK_STREAM, SOCKET_PROTOCOL);
     if(server->socket_fd < 0)
     {
-        fprintf(stderr, "Socket creation failed\n");
+        perror("socket");
         return SERVER_SOCKET_ERROR;
     }
 
@@ -22,19 +22,19 @@ server_status_e bind_tcp_port(tcp_server *server, int port)
 
     if(bind(server->socket_fd, (struct sockaddr *)&server->address, sizeof(server->address)) < 0)
     {
-        fprintf(stderr, "Bind failed\n");
+        perror("bind");
         close(server->socket_fd);
         return SERVER_LISTEN_ERROR;
     }
 
     if(listen(server->socket_fd, LISTEN_BACKLOG) < 0)
     {
-        fprintf(stderr, "Listen failed\n");
+        perror("listen");
         close(server->socket_fd);
         return SERVER_LISTEN_ERROR;
     }
 
-    fprintf(stderr, "Server bound and listening\n");
+    printf("Server bound and listening\n");
 
     return SERVER_OK;
 }
@@ -45,9 +45,9 @@ int accept_client(int server_fd)
     socklen_t client_len = sizeof(client_address);
 
     int client_fd = accept(server_fd, (struct sockaddr *)&client_address, &client_len);
-    if(client_fd)
+    if(client_fd < 0)
     {
-        fprintf(stderr, "Accept failed\n");
+        perror("accept");
         return STATUS_ERROR;
     }
 
